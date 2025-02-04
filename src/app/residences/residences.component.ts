21e812d2fdfc9e145dcd1bd6827f620fb4c0ca53
Harounjlassi <jlassi.haroun@esprit.tn>
2025-02-04 21:20:21 +0100

    add: filterSearch

--- src/app/residences/residences.component.ts
+++ src/app/residences/residences.component.ts
@@ -4,36 +4,66 @@ import { Residence } from '../model/Residence';
 @Component({
   selector: 'app-residences',
   templateUrl: './residences.component.html',
-  styleUrls: ['./residences.component.css']
+  styleUrls: ['./residences.component.css'],
 })
 export class ResidencesComponent {
-  listResidences:Residence[]=[
-    {id:1,"name": "El fel","address":"Borj Cedria",
-    "image":"../../assets/images/R1.png", status: "Disponible"},
-    {id:2,"name": "El yasmine",
-    "address":"Ezzahra","image":"../../assets/images/R2.png", status:
-    "Disponible" },
-    {id:3,"name": "El Arij",
-    "address":"Rades","image":"../../assets/images/R3.png", status:
-    "Vendu"},
-    {id:4,"name": "El Anber","address":"inconnu",
-    "image":"../../assets/images/R4.png", status: "En Construction"}
-    ];
-    listFav:Residence[]=[];
-    selectedAddress: string ='';
+  listResidences: Residence[] = [
+    {
+      id: 1,
+      name: 'El fel',
+      address: 'Borj Cedria',
+      image: '../../assets/images/R1.png',
+      status: 'Disponible',
+    },
+    {
+      id: 2,
+      name: 'El yasmine',
+      address: 'Ezzahra',
+      image: '../../assets/images/R2.png',
+      status: 'Disponible',
+    },
+    {
+      id: 3,
+      name: 'El Arij',
+      address: 'Rades',
+      image: '../../assets/images/R3.png',
+      status: 'Vendu',
+    },
+    {
+      id: 4,
+      name: 'El Anber',
+      address: 'inconnu',
+      image: '../../assets/images/R4.png',
+      status: 'En Construction',
+    },
+  ];
+  listFav: Residence[] = [];
+  selectedAddress: string = '';
+  search: string = '';
 
-    showAddress(address: string) {
-      if (address.toLowerCase() === 'inconnu') {
-        alert('Address is unknown!');
-      } else {
-        this.selectedAddress = address ;
+  showAddress(address: string) {
+    if (address.toLowerCase() === 'inconnu') {
+      alert('Address is unknown!');
+    } else {
+      this.selectedAddress = address;
+    }
+  }
+  searchText: string = '';
 
-      }
+
+  addFav(i: number) {
+    const index = this.listFav.findIndex((fav) => fav.id === i + 1);
+    if (index === -1) {
+      this.listFav.push(this.listResidences[i]);
+      console.log(i);
+    } else {
+      alert('This residence is already in  favorites!');
+    }
+  }
+
+  get filteredResidences() {
+    return this.listResidences.filter(residence =>
+      residence.address.toLowerCase().includes(this.searchText.toLowerCase())
+    );
 }
-
-addFav(i:number){
-this.listFav.push(this.listResidences[i]);
-
 }
-}
-
